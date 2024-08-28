e9a4139bfd75810c0b967d3ac4bb05ea7f9c925c
Olzhas <olzhas.dossaibek.dev@gmail.com>
2024-08-28 17:59:58 +0500

    115 - Building a Reusable Star Rating Component: p. 1/4 - created skeleton, first styles, and first props

--- src/App.js
+++ src/App.js
@@ -128,27 +128,6 @@ function Box({ children }) {
   );
 }
 
-// function WatchedBox() {
-//   const [isOpen2, setIsOpen2] = useState(true);
-
-//   return (
-//     <div className="box">
-//       <button
-//         className="btn-toggle"
-//         onClick={() => setIsOpen2((open) => !open)}
-//       >
-//         {isOpen2 ? "–" : "+"}
-//       </button>
-//       {isOpen2 && (
-//         <>
-//           <WatchedSummary watched={watched} />
-//           <WatchedMoviesList watched={watched}
-//         </>
-//       )}
-//     </div>
-//   );
-// }
-
 function MoviesList({ movies }) {
   return (
     <ul className="list">
