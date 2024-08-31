83e0bd108f972d0edc7a262af62fce8d6cca4a37
Olzhas <olzhas.dossaibek.dev@gmail.com>
2024-08-31 15:35:55 +0500

    119. Improving Reusability with Props - refactors StarRating and Star components for more reusability - props, default props, style, SVG fill, SVG stroke

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
+    <StarRating size={36} color="red" />
 
     {/* <App /> */}
   </React.StrictMode>
