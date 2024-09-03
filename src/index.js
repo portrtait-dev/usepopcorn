9cacae993c38ce0a9cb185636cbf4d1af05d82ef
Olzhas <olzhas.dossaibek.dev@gmail.com>
2024-09-03 17:34:19 +0500

    142. How NOT to Fetch Data in React - get API keys + create the first fetch request + show the problem with fetch() and setState() when the two are used together. Will resolve in the next lecture

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
-    <StarRating size={36} color="red" />
+    {/* <StarRating maxRating={5} /> */}
+    {/* <StarRating size={36} color="red" /> */}
 
-    {/* <App /> */}
+    <App />
   </React.StrictMode>
 );
