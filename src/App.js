be24fa2ba7cfb1900512b9a151b798370422f871
Olzhas <olzhas.dossaibek.dev@gmail.com>
2024-09-30 20:11:47 +0500

    156. Cleaning Up Data Fetching - Clean up data fetching at every typed query for better performance, UI update to exempt the new error - new AbortContoller(), controller.signal(), controller.abort()

--- src/App.js
+++ src/App.js
@@ -79,13 +79,16 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) {
@@ -97,9 +100,12 @@ export default function App() {
           if (data.Response === "False") throw new Error("Movie not found");
 
           setMovies(data.Search);
+          setError("");
         } catch (err) {
-          // console.error("----- " + err.message);
-          setError(err.message);
+          console.error(err.message);
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -112,6 +118,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
